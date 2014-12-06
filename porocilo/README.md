8e14a851b2d3e18d9f60c7249f0b63e62b06f950
unknown <vid.starc9@gmail.com>
2014-12-06 22:48:16 +0100

    popravek

--- porocilo/README.md
+++ porocilo/README.md
@@ -2,6 +2,7 @@
 Izbral sem si temo Število živine v Sloveniji in sicer bom predstavil število goveda in prašičev v različnih letih. Podatki so razdeljeni na vzhodno in zahodno Slovenijo. Poleg tega pa bom to število primerjal z številom goveda in prašičev po različnih državah Evrope. Podatke bom črpal iz statističnega urada republike Slovenije in iz uradne spletne strani Eurostat.
 
 Uvozil sem tabelo stevilo goveda in pri tem dodal še urejenostno spremenljivko ocena (primerjal sem slovensko povprečje in povprečje v štiriintridesetih državah v Evropi, torej ali je število posamezne vrste slabo zastopano, solidno ali odlično). Dodal sem stolpec povprečje pri kateremu sem izračunal vse povprečne vrednosti. V excelu sem dodal stolpec povprečje EU (ki obsega 34 držav v Evropi). Pri slednjem sem podatke črpal iz tabele, ki je na spletni strani Eurostat, seveda sem te povprečne vrednosti izračunal v Excelu. Tako pripravljeno tabelo (obsega 11 stolpcev in 69 vrstic) sem uvozil v program R, pri tem navedel še vse potrebne spremenljivke, imel pa sem težave z odpravljanjem znakov X v glavi tabele in pa znakov NA v tabeli.
-Uvozil sem tabelo stevilo prašičev iz datoteke stevilo-prasisev.csv, ki sem jo našel na spletni strani SURS.
+Uvozil sem tabelo stevilo prašičev iz datoteke stevilo-prasisev.csv, ki sem jo našel na spletni strani SURS in nato še tabelo v obliki html datokeke stevilo.ovac, ki sem ji tudi dobil na SURS-u.
+S pomočjo funkcije barplot sem narisal tri grafe in sicer število goveda v treh različnih letih ter jih shranil v dadoteko grafi.pdf
 
 Tukaj bo nastajalo poročilo o vašem projektu.
--- slike/grafi.r
+++ slike/grafi.r
@@ -1,4 +1,41 @@
 pdf("grafi.pdf")
 barplot(stevilo.goveda[,3],beside = TRUE, main="Število goveda v letu 2007", 
         xlab = "leto 2007", ylab = "število goveda",col = rainbow(30))
+barplot(stevilo.goveda[,4],beside = TRUE, main="Število goveda v letu 2008", 
+        xlab = "leto 2008", ylab = "število goveda",col = rainbow(30))
+barplot(stevilo.goveda[,5],beside = TRUE, main="Število goveda v letu 2009", 
+        xlab = "leto 2009", ylab = "število goveda",col = rainbow(30))
+
+barplot(stevilo.prasicev[1:15,3],beside = TRUE, main="Število prasicev v letu 2007-Slovenija", 
+        xlab = "leto 2007", ylab = "število prasicev",col = rainbow(30))
+legend("topright", c("Prašiči-SKUPAJ","Pujski,do 20kg-sesni","pujski,do 20kg-drugi",
+                     "Mladi Prašiči,20-50kg","Prašiči v pitanje-SKUPAJ","prašiči v pitanju,50-80kg","prašiči v pitanju,80-110kg",
+                     "prašiči v pitanju,110kg ali več","Plemenski prašiči 50kg-SKUPAJ","Plemenski prašiči nad 50kg,merjasci",
+                     "plemenski prašiči nad 50kg,nebreje mladice","plemenski prašiči nad 50kg,breje mladice",
+                     "plemenski prašiči nad 50kg,merjasci","plemenski prašiči nad 50kg,nebreje plemenske svinje",
+                     "plemenski prašiči nad 50kg,breje plemenske svinje"), fill = rainbow(30), cex=.60, title="LEGENDA",bg = "white")
+
+
+barplot(stevilo.prasicev[16:30,3],beside = TRUE, main="Število prasicev v letu 2007-Vzhodna Slo", 
+        xlab = "leto 2007", ylab = "število prasicev",col = rainbow(30))
+barplot(stevilo.prasicev[30:45,3],beside = TRUE, main="Število prasicev v letu 2007-Zahodna Slo", 
+        xlab = "leto 2007", ylab = "število prasicev",col = rainbow(30))
+
+#3D tortni diagram
+library(plotrix)
+ 
+slices <- stevilo.ovac[1:9,3] 
+lbls <- c(stevilo.ovac[1:9,2])
+pie3D(slices,labels=lbls,explode=0.2,main="stevilo ovac-SLOVENIJA")
+
+legend("topright", c("Ovce-SKUPAJ","Jagnjeta in mlade ovce",
+                     "Plemenske ovce-SKUPAJ",
+                     "plemenske ovce,prvič pripuščene,mlečne",
+                     "plemenske ovce,prvič pripuščene,druge",
+                     "plemenske ovce,ki so že jagnjile,mlečne",
+                     "plemenske ovce,ko se že jagnjile,druge",
+                     "Ovni","Jalove ovce"),
+       fill = rainbow(9), cex=.83, title="LEGENDA",text.font=0.5)
+       
+       
 dev.off()
